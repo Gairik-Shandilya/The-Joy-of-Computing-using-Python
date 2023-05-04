5e81165febccc3b8bc74e575ea4cc9c3113b036d
Gairik-Shandilya <104648687+Gairik-Shandilya@users.noreply.github.com>
2023-05-04 11:42:32 +0530

    Programming Assignment 1

--- Week 6/Aman likes to study about planets.py
+++ Week 6/Aman likes to study about planets.py
@@ -1,3 +1,7 @@
+# Aman likes to study about planets. Every night he goes outside and observe some planets with his telescope. 
+# Then he guesses the distance of each planet and pen it down. In this process he also pen down his favourite planet position. 
+# Given the distance of each planet to be unique you need to return position of Aman's favourite planet after sorting all the distances.
+
 n = int(input()) 
 L = list(map(int, input().split()))
 fav=L[int(input())-1]
