864d56a54cf9ba02916ac7c01aa8e8638f800c25
Gairik-Shandilya <104648687+Gairik-Shandilya@users.noreply.github.com>
2023-05-11 23:26:35 +0530

    Programming Assignment 3

---
+++ Week 10/Given a string s.py
@@ -0,0 +1,6 @@
+# Given a string s write a program to convert uppercase letters into lowercase and lowercase letters into uppercase. 
+# Also print the resultant string.
+# Note: You need to talk the input and do not print anything while taking input.
+
+line=input()
+print(line.swapcase())
