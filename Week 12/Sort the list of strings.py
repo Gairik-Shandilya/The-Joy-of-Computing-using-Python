f988893d64d25381de03b7e26c08b06f186e4c24
Gairik-Shandilya <104648687+Gairik-Shandilya@users.noreply.github.com>
2023-05-14 12:11:43 +0530

    Programming Assignment 2

---
+++ Week 12/Sort the list of strings.py
@@ -0,0 +1,10 @@
+#Given a list of strings, write a program to write sort the list of strings on the basis of last character of each string
+
+LSG=input().split()
+MI=[]
+for i in LSG:
+  MI.append("".join(list(i)[::-1]))
+ans=[]
+for i in sorted(MI):
+  ans.append("".join(list(i)[::-1]))
+print(ans,end="")    
